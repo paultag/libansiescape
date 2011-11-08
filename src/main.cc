70cad0767898b919bbda25d2e4d24ef3f27d1658
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-08 18:45:27 +0000

    Adding in a display for the mode

--- src/main.cc
+++ src/main.cc
@@ -10,12 +10,12 @@ int main ( int argc, char ** argv ) {
 	// char msg2[] = { 0x1B, '[', 'H',      '\0' };
 
 	char * msg = msg1;
-
 	ansi_sequence * seq1 = ansi_parse( msg );
 
 	for ( unsigned int i = 0; i < seq1->values->size(); ++i ) {
 		cout << seq1->values->at(i) << ", ";
 	}
+	cout << "Major mode: " << seq1->mode;
 	cout << endl;
 
 	delete seq1;
