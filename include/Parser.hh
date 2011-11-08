b9f8a09661a12ac32e04ae0e3af80292375870ca
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-08 12:30:22 -0500

    Adding the return struct type

--- include/Parser.hh
+++ include/Parser.hh
@@ -1,6 +1,13 @@
 #ifndef _PARSER_HH_
 #define _PARSER_HH_ I_CAN_HAS
 
+#include <vector>
+
+typedef struct _ansi_sequence {
+	char             mode;
+	std::vector<int> values; // XXX: Technically wrong
+} ansi_sequence;
+
 void ansi_validate( char * sequence );
 
 #endif
