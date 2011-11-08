5a5a3ebd1aeb1531d53fe2f67d76c87ecd1ab116
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-08 18:07:51 +0000

    implementing the ender a bit

--- src/CSIEnder.cc
+++ src/CSIEnder.cc
@@ -8,25 +8,29 @@
 #include <iostream>
 
 #include "StateMachine.hh"
+#include "Exception.hh"
+#include "InvalidState.hh"
 #include "CSIEnder.hh"
 #include "CSIValue.hh"
+#include "ANSIEntry.hh"
+
+/* Some wikipedia background:
+ * The final byte is technically any character in the range 64 to 126
+ * (hex 0x40 to 0x7e, ASCII @ to ~), and may be modified extended with leading
+ * intermediate bytes in the range 32 to 47 (hex 0x20 to 0x2f). */
 
 void CSIEnder::feed( char c ) {
-
-}
-
-void CSIEnder::enter() {
-	for (
-		unsigned int i = 0;
-		i < ansi_state_CSIValue_parsed_ints.size();
-		++i
-	) {
-		std::cout << ansi_state_CSIValue_parsed_ints.at(i) << ", " << std::endl;
+	/* XXX: Implement leading bytes correctly */
+	if ( c >= 64 && c <= 126 ) {
+		/* We've parsed well */
+		ansi_next_state = &ansi_state_ANSIEntry;
+	} else {
+		ansi_next_state = &ansi_state_InvalidState;
+		throw new InvalidSequence();
 	}
 }
 
-void CSIEnder::exit() {
-
-}
+void CSIEnder::enter() {}
+void CSIEnder::exit()  {}
 
 CSIEnder ansi_state_CSIEnder;
