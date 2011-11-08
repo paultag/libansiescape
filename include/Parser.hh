3a4fa2c1d27bbb43775094be8614f65230d279f5
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-08 12:26:35 -0500

    Adding in some parsing external interfaces. We'll build this out and test it.

---
+++ include/Parser.hh
@@ -0,0 +1,6 @@
+#ifndef _PARSER_HH_
+#define _PARSER_HH_ I_CAN_HAS
+
+void ansi_validate( char * sequence );
+
+#endif
---
+++ src/Parser.cc
@@ -0,0 +1,5 @@
+#include "Parser.hh"
+
+void ansi_validate ( char * sequence ) {
+
+}
