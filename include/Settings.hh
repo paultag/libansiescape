ce1a9b242e9125ec8874f0787648f8efa9b4d307
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-08 15:14:13 +0000

    Nice. Including Settings + rewriting some krufty kruft.

---
+++ include/Settings.hh
@@ -0,0 +1,17 @@
+#ifndef _SETTINGS_HH_
+#define _SETTINGS_HH_ PH34R_M3
+
+#define _APPNAME_ "libansi"
+#define _LICENSE_ "MIT"
+
+#define _AUTHOR_  "Paul R. Tagliamonte <tag@pault.ag>"
+#define _YEAR_    "2011"
+#define _VERSION_ "0.8~pre1"
+
+/* OK. Now that legal stuff is all up on the top, we
+   can do some of the more "fun" settings. */
+
+#define CSI_EMPTY_VALUE  -1
+/* Please ensure this is an int. */
+
+#endif
--- src/CSIValue.cc
+++ src/CSIValue.cc
@@ -8,7 +8,22 @@
 #include "StateMachine.hh"
 #include "CSIValue.hh"
 
+#include "Settings.hh"
+
 void CSIValue::feed( char c ) {
+	/* OK. C must be one of the following:
+	 *   0-9, ;
+	 *   ^    ^
+	 *    \    \
+	 *     \    +---- Delim  - push back chars we've got
+	 *      +-------- Number - push into parsed holder, str to int on delim
+	 *  If the holder is empty and we hit a delim, we push back -1, since
+	 *  -1 is technically invalid. We could also use null, so we'll make this
+	 *  compile-time conf-able.
+	 *
+	 *  Setting is in: Settings.hh/CSI_EMPTY_VALUE
+	 * 
+	 */
 
 }
 
