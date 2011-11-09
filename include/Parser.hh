3ded50bae8c16e87af0d9776711eb951ced832a1
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-09 17:41:44 +0000

    Adding in the new header + impl, removing an old function.

--- include/Parser.hh
+++ include/Parser.hh
@@ -33,7 +33,6 @@ class ansi_sequence {
 		std::vector<int> * values;
 };
 
-void ansi_validate( char * sequence );
 ansi_sequence * ansi_parse( char * sequence );
 
 extern ansi_sequence * ansi_parser_last_parsed;
---
+++ include/ansiescape.hh
@@ -0,0 +1,9 @@
+#ifndef _ANSIESCAPE_HH_
+#define _ANSIESCAPE_HH_
+
+/* This header is to give folks a clean and simple facade to interface with
+ * the ansi library. Hopefully we can keep this stable and maintainable. */
+
+
+
+#endif
