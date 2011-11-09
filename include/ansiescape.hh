54196a94b87d454309b2e50720dfdf1e33150a05
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-09 17:45:32 +0000

    Updating the interface a skitch

--- include/ansiescape.hh
+++ include/ansiescape.hh
@@ -4,6 +4,15 @@
 /* This header is to give folks a clean and simple facade to interface with
  * the ansi library. Hopefully we can keep this stable and maintainable. */
 
+#define ANSI_ESCAPE_PARSE_OK         1
+#define ANSI_ESCAPE_PARSE_BAD        2
+#define ANSI_ESCAPE_PARSE_INCOMPLETE 3
+#define ANSI_ESCAPE_PARSE_T          int
 
+ANSI_ESCAPE_PARSE_T ansi_escape_parser_feed( char c );
+
+void ansi_escape_parse_string( std::string  s );
+void ansi_escape_parse_string( char *       c );
+void ansi_escape_parse_string( const char * c
 
 #endif
