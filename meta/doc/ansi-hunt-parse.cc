a1450d97b6633f49585f322a62ba07df08e45c34
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-10 13:17:26 +0000

    Showing off the new header-file

--- meta/doc/ansi-hunt-parse.cc
+++ meta/doc/ansi-hunt-parse.cc
@@ -1,32 +1,7 @@
-#include <ansi/Parser.hh>
-#include <ansi/ANSIEntry.hh>
-#include <ansi/StateMachine.hh>
-#include <ansi/Exception.hh>
+#include <ansiescape.hh>
 
 #include <string.h>
 #include <iostream>
-
-#define PARSE_OK         1
-#define PARSE_NO         2
-#define PARSE_INCOMPLETE 3
-
-void reset_parser() {
-	ansi_next_state = &ansi_state_ANSIEntry;
-	ansi_state_init();
-}
-
-int feed( char c ) {
-	try {
-		ansi_state_process( c );
-		if ( ansi_next_state == &ansi_state_ANSIEntry ) {
-			return PARSE_OK;
-		}
-		return PARSE_INCOMPLETE;
-
-	} catch ( InvalidSequence * seq ) {
-		return PARSE_NO;
-	}
-}
 
 void output( ansi_sequence * holder ) {
 	for ( unsigned int i = 0; i < holder->values->size(); ++i ) {
@@ -37,22 +12,22 @@ void output( ansi_sequence * holder ) {
 }
 
 int main ( int argc, char ** argv ) {
-	reset_parser();
-	ansi_sequence * holder;
+	ansi_escape_parser_reset();
+	ansi_sequence * holder = NULL;
 
 	char sequence[] = { 'h', 'e', 0x1B, '[', 'J', 'l', 'l', 'o', '\0' };
 
 	for ( unsigned int i = 0; i < strlen( sequence ); ++i ) {
-		int ret = feed( sequence[i] );
+		int ret = ansi_escape_parser_feed( sequence[i] );
 		switch ( ret ) {
-			case PARSE_OK:
+			case ANSI_ESCAPE_PARSE_OK:
 				holder = ansi_parser_last_parsed;
 				output( holder );
 				break;
-			case PARSE_NO:
-				reset_parser();
+			case ANSI_ESCAPE_PARSE_BAD:
+				ansi_escape_parser_reset();
 				break;
-			case PARSE_INCOMPLETE:
+			case ANSI_ESCAPE_PARSE_INCOMPLETE:
 				/* No action */
 				break;
 		}
