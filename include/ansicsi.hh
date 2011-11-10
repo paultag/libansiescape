bfd0c514bc90d901dd8d592dc568bea0a1f32516
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-10 19:48:01 +0000

    adding in CSI aliases

---
+++ include/ansicsi.hh
@@ -0,0 +1,22 @@
+#ifndef _ANSICSI_HH_
+#define _ANSICSI_HH_ CAN_HAS
+
+#define CSI_CUP    'A'
+#define CSI_CUD    'B'
+#define CSI_CUF    'C'
+#define CSI_CUB    'D'
+#define CSI_CNL    'E'
+#define CSI_CPL    'F'
+#define CSI_CHA    'G'
+// #define CSI_CUP  /* XXX: Better solution, please */
+#define CSI_ED     'J'
+#define CSI_EL     'K'
+#define CSI_SU     'S'
+#define CSI_SD     'T'
+#define CSI_HVP    'f'
+#define CSI_SGR    'm'
+#define CSI_DSR    'n'
+#define CSI_SCP    's'
+#define CSI_RCP    'u'
+
+#endif
